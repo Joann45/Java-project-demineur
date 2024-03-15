ac4475f108c319c33f6eeea6302581d164fe8b89
Joann45 <joann.raignault@gmail.com>
2024-03-15 17:00:20 +0100

    CaseIntelligente

--- CaseIntelligente.java
+++ CaseIntelligente.java
@@ -5,18 +5,40 @@ public class CaseIntelligente extends Case {
     private List<Case> lesVoisines;
 
     public CaseIntelligente(){
+        super();
         this.lesVoisines = new ArrayList<>();
     }
     public void ajouteVoisine(Case uneCase){
-
+        this.lesVoisines.add(uneCase);
     }
     public int nombreBombesVoisines(){
-        return 1;
+        int nbBombes = 0;
+        for (Case uneCase : this.lesVoisines){
+            if (uneCase.contientUneBombe()){
+                nbBombes += 1;
+            }
+        }
+        return nbBombes;
     }
 
     @Override
 
     public String toString(){
-        return "";
+        String symboleCase = "";
+        if (!this.estDecouverte()){
+            symboleCase = " ";
+            if (this.estMarquee()){
+                symboleCase = "?";
+            }
+        }
+        else {
+            if (this.contientUneBombe()){
+                symboleCase = "@";
+            }
+            else {
+                symboleCase = ""+this.nombreBombesVoisines();
+            }
+        }
+        return symboleCase;
     }
 }
