5c88bb029509d9afd6cca02e3ae91817be649397
Joann45 <joann.raignault@gmail.com>
2024-03-25 11:16:24 +0100

    ajout de méthodes dans plateau

--- Plateau.java
+++ Plateau.java
@@ -8,16 +8,51 @@ public class Plateau{
     private int nbColonnes;
     private int pourcentageDeBombes;
     private int nbBombes;
-    private List<CaseIntelligente> lePlateau;
+    private List<List<CaseIntelligente>> lePlateau;
 
     public Plateau(int nbLignes, int nbColonnes, int pourcentageDeBombes){
-        
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.pourcentageDeBombes = pourcentageDeBombes;
+        this.nbBombes = nbLignes*nbColonnes*pourcentageDeBombes/100;
+        this.lePlateau = new ArrayList<>();
     }
+
+    public CaseIntelligente getCase(int numLigne, int numColonne) throws IndexOutOfBoundsException{
+        return this.lePlateau.get(numLigne).get(numColonne);
+    }
+
     private void creerLesCasesVides(){
+        List<CaseIntelligente> uneLigne = new ArrayList<>();
+        for (int i = 0; i < this.nbLignes; i++){
+            uneLigne.clear();
+            for (int j = 0; j < this.nbColonnes; j++) {
+                uneLigne.add(new CaseIntelligente());
+            }
+            this.lePlateau.add(uneLigne);
+        }
 
     }
-    private void rendLesCasesIntelligentes(){
+    private void rendLesCasesIntelligentesArrêtes(int i, int j){
+        if (i == 0){
+            if (j == 0){
+                getCase(i, j).ajouteVoisine(getCase(i, j+1));
+                getCase(i, j).ajouteVoisine(getCase(i+1, j));
+                System.exit(0);
+            }
+            getCase(i, j).ajouteVoisine(getCase(i, j+1));
+            getCase(i, j).ajouteVoisine(getCase(i+1, j));
+            getCase(i, j).ajouteVoisine(getCase(i, j-1));
+            System.exit(0);
+        }
+    }
 
+    private void rendLesCasesIntelligentes(){
+        for (int i = 0; i < this.lePlateau.size(); i++){
+            for (int j = 1; j < this.lePlateau.get(0).size(); i++){
+                
+            }
+        }
     }
     protected void poseDesBombesAleatoirement(){
         Random generateur = new Random();
@@ -39,17 +74,30 @@ public class Plateau{
     public int getNbTotalBombes(){
         return 1;
     }
-    public CaseIntelligente getCase(int numLigne, int numColonne){
-        return new CaseIntelligente();
-    }
-    public int getNbCasesMarquees(){
-        return 1;
-    }
-    public void poseBombe(int x, int y){
 
+    public int getNbCasesMarquees(){
+        int n = 0;
+        for (int i = 0; i<this.getNbLignes(); i ++){
+            for (int j = 0; j < this.getNbColonnes(); j++){
+                if (this.getCase(i, j).estMarquee()){
+                    n += 1;
+                }
+            }
+        }
+        return n;
     }
+
+    public void poseBombe(int x, int y){
+        this.getCase(x, y).poseBombe();
+    }
+
     public void reset(){
-        
+        this.nbBombes = 0;
+        for (int i = 0; i<this.getNbLignes(); i ++){
+            for (int j = 0; j < this.getNbColonnes(); j++){
+                this.getCase(i, j).reset();
+            }
+        }
     }
 
 }
